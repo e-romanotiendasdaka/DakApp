352c31b4e6988629f201f32000ac7af916cf871c
elvisromano14 <40697832+elvisromano14@users.noreply.github.com>
2023-07-17 08:22:34 -0400

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once "controladores/plantilla.controlador.php";
+require_once "controladores/maestros.controlador.php";
+require_once "controladores/usuarios.controlador.php";
+
+require_once "modelos/conexiones.modelo.php";
+require_once "modelos/maestros.modelo.php";
+require_once "modelos/serializacion.modelo.php";
+
+$plantilla = new ControladorPlantilla();
+$plantilla -> ctrPlantilla();
